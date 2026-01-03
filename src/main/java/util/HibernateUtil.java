f6c1b18bfb7345335741202ef0fcfacebe79706d
Nethindu Hansaka <nethinduhansaka780@gmail.com>
2026-01-03 09:34:08 +0530

    modified:   src/main/java/util/HibernateUtil.java 	modified:   target/classes/util/HibernateUtil.class

--- src/main/java/util/HibernateUtil.java
+++ src/main/java/util/HibernateUtil.java
@@ -25,7 +25,7 @@ public class HibernateUtil {
                 settings.put(Environment.URL,
                         "jdbc:mysql://localhost:3306/university_db?useSSL=false");
                 settings.put(Environment.USER, "root");
-                settings.put(Environment.PASS, "root");
+                settings.put(Environment.PASS, "Password");
 
                 settings.put(Environment.DIALECT,
                         "org.hibernate.dialect.MySQL8Dialect");
